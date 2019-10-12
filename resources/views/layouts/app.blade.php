5f8f96dde49538d85516f8a00d5acf01e5897bd8
li-hp <l-ihp@qq.com>
2019-10-12 00:47:52 +0000

    lhp  xuyao quanpin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,31 +15,43 @@
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-
+  
+  <link href="css/index_style.css" rel="stylesheet" type="text/css">
   @yield('styles')
 
 </head>
 
 <body>
+<div class="header" id="demo">
+  
+  <div class="nav">
 
+ <div class="container-fulid">
         @include('layouts._header')
-    
-        <div class="container-fulid">
+       
+       
     
           @yield('content')
     
         </div>
     
         @include('layouts._footer')
+        </div>
+    
 
+
+  
+    <canvas id="canvas"></canvas>
+  
 
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
+  <script type="text/javascript" src="js/bg.js"></script> 
   <script type="text/javascript" src="js/china.js"></script> 
   <script type="text/javascript" src="js/area_echarts.js"></script> 
   {{-- <script type="text/javascript" src="js/js1.js"></script>  --}}
   @yield('scripts')
-
+  </div>
 </body>
 
 </html>
