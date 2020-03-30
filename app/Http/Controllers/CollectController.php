f26ba6b5a9c443203824506849848d0ff4199dbd
biaogeniubi666 <365056996@qq.com>
2020-03-30 11:14:38 +0800

    123

--- app/Http/Controllers/CollectController.php
+++ app/Http/Controllers/CollectController.php
@@ -10,7 +10,7 @@ class CollectController extends Controller
 {
     //实时数据采集
     public function datathismoment() {
-        // 设置时区
+        // 设置时区,待优化，写入php.ini文件内
         date_default_timezone_set('PRC');
 
         $py_data = ppython2("lanju_inside::Lanju_inside");
@@ -22,22 +22,45 @@ class CollectController extends Controller
 
     // 7小时历史数据
     public function data7hour () {
-        $hourtest = DB::table('lanju_insides')->latest('SERVER_TIME')
-            ->where('SERVER_TIME', '>', date('Y-m-d H:i:s',strtotime('-7 day')))
+        date_default_timezone_set('PRC');
+        $history_hour = DB::table('lanju_insides')->latest('SERVER_TIME')
+            ->where('SERVER_TIME', '>', date('Y-m-d H:i:s',strtotime('-7 hour')))
             ->where('SERVER_TIME', '<=', date('Y-m-d H:i:s',strtotime('now')))
-            ->limit(10800)->get();
+            ->limit(420)->get();
 
-        return $hourtest;
+        return $history_hour;
     }
 
     //7日历史数据
     public function  data7day () {
-        $day_data = LanjuInside::
-        whereDate('SERVER_TIME', '>', date('Y-m-d',strtotime('-2 day')))
-            ->whereDate('SERVER_TIME', '<', date('Y-m-d',strtotime('now')))
-            ->pluck('SERVER_TIME');
-//        return $day_data;
+        date_default_timezone_set('PRC');
+        $history_day = DB::table('lanju_insides')->latest('SERVER_TIME')
+            ->where('SERVER_TIME', '>', date('Y-m-d H:i:s',strtotime('-7 day')))
+            ->where('SERVER_TIME', '<=', date('Y-m-d H:i:s',strtotime('now')))
+            ->limit(10800)->get();
+
+        return $history_day;
     }
 
+    public function axis () {
+        date_default_timezone_set('PRC');
+        $axis7days = [date('Y-m-d H:i:s',strtotime('-6 day')),
+            date('Y-m-d H:i:s',strtotime('-5 day')),
+            date('Y-m-d H:i:s',strtotime('-4 day')),
+            date('Y-m-d H:i:s',strtotime('-3 day')),
+            date('Y-m-d H:i:s',strtotime('-2 day')),
+            date('Y-m-d H:i:s',strtotime('-1 day')),
+            date('Y-m-d H:i:s',strtotime('now'))];
+
+        $axis7hour = [date('Y-m-d H:i:s',strtotime('-6 hour')),
+            date('Y-m-d H:i:s',strtotime('-5 hour')),
+            date('Y-m-d H:i:s',strtotime('-4 hour')),
+            date('Y-m-d H:i:s',strtotime('-3 hour')),
+            date('Y-m-d H:i:s',strtotime('-2 hour')),
+            date('Y-m-d H:i:s',strtotime('-1 hour')),
+            date('Y-m-d H:i:s',strtotime('now'))];
+        return [$axis7days,$axis7hour];
+
+    }
 }
 
